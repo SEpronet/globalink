730107f3aa0a3f8f343e8c0ea015f9479c0c5dfd
Kevin <hk.at.dang@gmail.com>
2016-10-25 13:52:48 -0700

    Added profile view

--- db/migrate/20160916084653_add_firstname_to_users.rb
+++ db/migrate/20160916084653_add_firstname_to_users.rb
@@ -4,6 +4,5 @@ class AddFirstnameToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :lastname, :string
     add_column :users, :title, :string
     add_column :users, :education, :string
-    
   end
 end
