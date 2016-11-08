f52d7116bf0231daa180ff7659dc9b7c2a2df913
Akshat Sharma <akshatsharma46@gmail.com>
2016-11-08 13:22:45 -0800

    Working Profile and MyConnect

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, styles: { largem: "600x600>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  has_many :friendships
+has_many :friends, :through => :friendships
+has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 end
